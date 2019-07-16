cb3b01a1004c241db7e45436856a1f5161ca35b5
Ray Ranola <ray.ranola@vmlyr.com>
2019-07-16 17:37:15 +1000

    style: (base: prettier everything, plus added themes)

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'sanitize.css';
+import '../themes/v1/scss/style.scss';
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<h1>This is a blank app.</h1>
+		</div>
+	);
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,19 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 const history = createBrowserHistory();
 const store = configureStore({}, history);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root'),
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
+	</Provider>,
+	document.getElementById('root'),
 );
 
 serviceWorker.unregister();
